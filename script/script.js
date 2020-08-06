849d838a1cf44796f48ac17c8824cfb705d2963b
Lavezur <artemeremenko.m@yandex.ru>
2020-08-07 00:57:02 +0300

    *Третья попытка сдачи 4-ой *жабки в очках* Яндекс.Практикум*
    		<-- Исправленые ошибки -->
    - Все комментарии с пометкой "Нужно исправить"
    - Кнопки исправлены
    - JS исправлен
    - В чем ошибка в высоте? https://imgur.com/Jnubvvv на макете 361px в работе было чуть меньше, теперь ровно 361px ! * грустная жабка в очках *
    - А еще я сделала более человеческий адаптив для 552px и 398px ! *жабка в очках*
    
    *жабка в очках*
    https://yt3.ggpht.com/a/AATXAJwBQju9CnGiXETIeMQ6e9W2NCtwVPXgEm2SmJmOGg=s900-c-k-c0xffffffff-no-rj-mo

--- script/script.js
+++ script/script.js
@@ -1,54 +1,32 @@
 let popup = document.querySelector('.pop-up');
 let popupOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = popup.querySelector('.pop-up__close-button');
-
 let formElement = document.querySelector('.pop-up__form');
 let profile = document.querySelector('.profile');
+let nameInput = formElement.querySelector('.pop-up__name');
+let jobInput = formElement.querySelector('.pop-up__job');
+let nameProfile = profile.querySelector('.profile__name');
+let jobProfile = profile.querySelector('.profile__about');
 
+function popupOpen() {
+    nameInput.value = nameProfile.textContent;
+    jobInput.value = jobProfile.textContent;
 
-function popupToggle() {
-    popup.classList.toggle('pop-up_open');
-
-    let nameProfile = profile.querySelector('.profile__name');
-    let jobProfile = profile.querySelector('.profile__about');
-
-
-    let nameValue = nameProfile.textContent;
-    let jobValue = jobProfile.textContent;
-
-    let nameInput = formElement.querySelector('.pop-up__name');
-    let jobInput = formElement.querySelector('.pop-up__job');
-
-    nameInput.value = nameValue;
-    jobInput.value = jobValue;
+    popup.classList.add('pop-up_open');
 }
 
-popupOpenButton.addEventListener('click', popupToggle)
-popupCloseButton.addEventListener('click', popupToggle)
+function popupClose() {
+    popup.classList.remove('pop-up_open');
+}
 
+popupOpenButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-    function formSubmitHandler (evt) {
-        evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-        // Так мы можем определить свою логику отправки.
-        // О том, как это делать, расскажем позже.
+function formSubmitHandler (evt) {
+        evt.preventDefault();
 
-        // Находим поля формы в DOM
-        let nameInput = formElement.querySelector('.pop-up__name');
-        let jobInput = formElement.querySelector('.pop-up__job');
+        nameProfile.textContent = nameInput.value;
+        jobProfile.textContent = jobInput.value;
+}
 
-        // Получите значение полей из свойства value
-        let nameValue = nameInput.value;
-        let jobValue = jobInput.value;
-        // Выберите элементы, куда должны быть вставлены значения полей
-        let nameProfile = profile.querySelector('.profile__name');
-        let jobProfile = profile.querySelector('.profile__about');
-        // Вставьте новые значения с помощью textContent
-        nameProfile.textContent = nameValue;
-        jobProfile.textContent = jobValue;
-    }
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
