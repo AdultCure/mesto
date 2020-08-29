7c44c78f1496b26e3014ad150d438777996c9804
Lavezur <artemeremenko.m@yandex.ru>
2020-08-29 15:52:20 +0300

    save

--- script/script.js
+++ script/script.js
@@ -2,8 +2,8 @@
 const popup = document.querySelector('.pop-up');
 const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = popup.querySelector('.pop-up__close-button');
-const formElement = document.querySelector('.pop-up__form');
 const profile = document.querySelector('.profile');
+const formElement = document.querySelector('.pop-up__form');
 const nameInput = formElement.querySelector('.pop-up__name');
 const jobInput = formElement.querySelector('.pop-up__job');
 const nameProfile = profile.querySelector('.profile__name');
@@ -36,6 +36,7 @@ popupOpenButton.addEventListener('click', function() {
 
     popupOpen(nameProfOpen)
 });
+
 popupCloseButton.addEventListener('click', function() {
     nameProfClose = document.querySelector('.pop-up')
 
@@ -55,7 +56,6 @@ popPictureCloseButton.addEventListener('click', function() {
 });
 
 
-
 function formSubmitHandler (evt) {
         evt.preventDefault();
         nameProfile.textContent = nameInput.value;
@@ -67,7 +67,9 @@ function formSubmitHandler (evt) {
 
 formElement.addEventListener('submit', formSubmitHandler);
 
-
+/*----------------------------------------------------------------------------------------------------------------------*/
+/*Страшно*/
+/*----------------------------------------------------------------------------------------------------------------------*/
 
  const initialCards = [
     {
@@ -96,7 +98,6 @@ formElement.addEventListener('submit', formSubmitHandler);
     }
 ];
 
-/*----------------------------------------------------------------------------------------------------------------------*/
 
 const addToCard = (cardTitle, cardUrl) => {
     const cardTemplate = document.querySelector('#card').content.cloneNode(true)
@@ -143,7 +144,6 @@ renderCard = (title, src) => {
     elementsContainer.prepend(addToCard(title, src))
 }
 
-
 popPictureFormElement.addEventListener('submit', event => {
     event.preventDefault()
     popPictureClose = document.querySelector('.pop-picture')
@@ -159,10 +159,100 @@ initialCards.forEach(item => {
     renderCard(item.name, item.link)
 });
 
-
-
+/*----------------------------------------------------------------------------------------------------------------------*/
+/* форма Профиля */
 /*----------------------------------------------------------------------------------------------------------------------*/
 
+const showError = (formItem, input, errorMessage) => {
+    const errorElement = formItem.querySelector(`#${input.id}-error`);
+    input.classList.add('pop-up__type-error');
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add('form__input-error_active');
+}
+
+const hideError = (formItem, input) => {
+    const errorElement = formItem.querySelector(`#${input.id}-error`);
+    input.classList.remove('pop-up__type-error');
+    errorElement.classList.remove('form__input-error_active');
+    errorElement.textContent = '';
+}
+
+const checkInputValidity = (formItem, input) => {
+    if (!input.validity.valid) {
+        showError(formElement, input, input.validationMessage);
+    } else {
+        hideError(formElement, input);
+    }
+};
+/* ---------------------------------------------------- */
+
+const checkInputValidityPicture = (formItem, input) => {
+    if (!input.validity.valid) {
+        showError(popPictureFormElement, input, input.validationMessage)
+    } else {
+        hideError(popPictureFormElement, input);
+    }
+}
 
 
+const setEventListenersPicture = (formItem) => {
+    const inputList = Array.from(formItem.querySelectorAll('.form__input-picture'));
+    const buttonElement = formElement.querySelector('.pop-picture__save-button');
+
+    toggleButtonState(inputList, buttonElement);
+
+    inputList.forEach((input) => {
+        input.addEventListener('input', function() {
+            checkInputValidityPicture(formItem, input);
+
+            toggleButtonState(inputList, buttonElement)
+        });
+    });
+}
+
+/* ---------------------------------------------------- */
+
+const setEventListeners = (formItem) => {
+    const inputList = Array.from(formItem.querySelectorAll('.form__input'));
+    const buttonElement = formElement.querySelector('.pop-up__save-button');
+
+    toggleButtonState(inputList, buttonElement);
+
+    inputList.forEach((input) => {
+        input.addEventListener('input', function () {
+            checkInputValidity(formItem, input);
+
+            toggleButtonState(inputList, buttonElement);
+        });
+    });
+}
+
+
+function enableValidation() {
+    formList = Array.from(document.querySelectorAll('.pop-up__form'));
+    formList.forEach((formItem) => {
+        formItem.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+        })
+        setEventListeners(formItem)
+    })
+}
+enableValidation()
+
+function hasInvalidInput (inputList) {
+    return inputList.some((input) => {
+        return !input.validity.valid;
+    })
+}
+
+function toggleButtonState (inputList, buttonElement) {
+    if (hasInvalidInput(inputList)) {
+        buttonElement.classList.add('pop-up__save-button_inactive');
+    } else {
+        buttonElement.classList.remove('pop-up__save-button_inactive');
+    }
+}
+/*----------------------------------------------------------------------------------------------------------------------*/
+/* форма Новой карточки */
+/*----------------------------------------------------------------------------------------------------------------------*/
 
